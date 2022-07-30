97cc180d534cdff4663cf515a044059cdf1475f0
Daniel Bettles <danbettles@yahoo.co.uk>
2022-07-30 12:46:09 +0000

    Updated PHPDoc.

--- src/Utils/FilesystemUtils.php
+++ src/Utils/FilesystemUtils.php
@@ -14,11 +14,12 @@ use const PATHINFO_EXTENSION;
 class FilesystemUtils
 {
     /**
-     * PATHINFO_ALL:
+     * `PATHINFO_ALL`:
      *   The array will always contain the `extension` element: the value will be `null` if the pathname contains no
-     *   extension.  (The value will be an empty string if the pathname has a blank extension.)
-     * PATHINFO_EXTENSION:
-     *   `null` if the pathname contains no extension.  (An empty string if the pathname has a blank extension.)
+     *   extension, or an empty string if the pathname has a blank extension.
+     *
+     * `PATHINFO_EXTENSION`:
+     *   `null` if the pathname contains no extension, or an empty string if the pathname has a blank extension.
      *
      * @return array|string|null
      */
