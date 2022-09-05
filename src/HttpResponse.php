f05576a6479dd87fb46a6c3dddd77e4d2a02d494
Daniel Bettles <dan@powder-blue.com>
2022-09-05 19:24:01 +0100

    Added a very basic HTTP-response class, 'HttpResponse'.

---
+++ src/HttpResponse.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DanBettles\Marigold;
+
+use function header;
+
+use const null;
+
+class HttpResponse
+{
+    /** @var int */
+    public const HTTP_OK = 200;
+    /** @var int */
+    public const HTTP_NOT_FOUND = 404;
+    /** @var int */
+    public const HTTP_INTERNAL_SERVER_ERROR = 500;
+
+    /** @var array<int, string> */
+    private const STATUS_TEXTS = [
+        self::HTTP_OK => 'OK',
+        self::HTTP_NOT_FOUND => 'Not Found',
+        self::HTTP_INTERNAL_SERVER_ERROR => 'Internal Server Error',
+    ];
+
+    private int $statusCode;
+
+    private string $content;
+
+    public function __construct(
+        string $content = '',
+        int $statusCode = self::HTTP_OK
+    ) {
+        $this
+            ->setContent($content)
+            ->setStatusCode($statusCode)
+        ;
+    }
+
+    protected function sendHeader(string $name, string $value = null): void
+    {
+        $header = null === $value
+            ? $name
+            : "{$name}: {$value}"
+        ;
+
+        header($header);
+    }
+
+    /**
+     * @param array<string, string> $server
+     */
+    public function send(array $server): void
+    {
+        $statusHeader = (
+            ($server['SERVER_PROTOCOL'] ?? 'HTTP/1.0')
+            . ' '
+            . (string) $this->getStatusCode()
+            . ' '
+            . self::STATUS_TEXTS[$this->getStatusCode()]
+        );
+
+        $this->sendHeader($statusHeader);
+
+        // phpcs:ignore
+        echo $this->getContent();
+    }
+
+    private function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    private function setStatusCode(int $statusCode): self
+    {
+        $this->statusCode = $statusCode;
+        return $this;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+}
---
+++ tests/src/HttpResponseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DanBettles\Marigold\Tests;
+
+use DanBettles\Marigold\AbstractTestCase;
+use DanBettles\Marigold\HttpResponse;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class HttpResponseTest extends AbstractTestCase
+{
+    public function testIsInstantiable(): void
+    {
+        $emptyResponse = new HttpResponse();
+        $this->assertSame('', $emptyResponse->getContent());
+        $this->assertSame(200, $emptyResponse->getStatusCode());
+
+        $emptyResponse = new HttpResponse('foo');
+        $this->assertSame('foo', $emptyResponse->getContent());
+        $this->assertSame(200, $emptyResponse->getStatusCode());
+
+        $emptyResponse = new HttpResponse('bar', 404);
+        $this->assertSame('bar', $emptyResponse->getContent());
+        $this->assertSame(404, $emptyResponse->getStatusCode());
+
+        $emptyResponse = new HttpResponse('baz', 500);
+        $this->assertSame('baz', $emptyResponse->getContent());
+        $this->assertSame(500, $emptyResponse->getStatusCode());
+    }
+
+    public function testSendSendsTheResponseToTheClient(): void
+    {
+        $this->expectOutputString('An unexpected error occurred.');
+
+        /** @var MockObject */
+        $responseMock = $this
+            ->getMockBuilder(HttpResponse::class)
+            ->onlyMethods(['sendHeader'])
+            ->setConstructorArgs([
+                'An unexpected error occurred.',
+                500
+            ])
+            ->getMock()
+        ;
+
+        $responseMock
+            ->expects($this->once())
+            ->method('sendHeader')
+            ->with('HTTP/1.1 500 Internal Server Error')
+        ;
+
+        /** @var HttpResponse $responseMock */
+        $responseMock->send([
+            'SERVER_PROTOCOL' => 'HTTP/1.1',
+        ]);
+    }
+}
